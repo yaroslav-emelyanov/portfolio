d747d92f83587c85070198ddfa3c60c8800f7a79
yaroslav-emelyanov <emelyanov.yaroslav.97@gmail.com>
2020-05-04 22:17:49 +0300

    Updates

---
+++ precache-manifest.64be50c9ae50b3507eda6da114dc9f4f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6f478e0373cced49adff6d27ef7cc6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d17c1f928006bc7e8a7e",
+    "url": "/portfolio/static/css/main.6bea0958.chunk.css"
+  },
+  {
+    "revision": "ba6e305519debfff7738",
+    "url": "/portfolio/static/js/2.81b288e8.chunk.js"
+  },
+  {
+    "revision": "505a6f6bdacd226f93a956d35104ccd6",
+    "url": "/portfolio/static/js/2.81b288e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17c1f928006bc7e8a7e",
+    "url": "/portfolio/static/js/main.6633dde4.chunk.js"
+  },
+  {
+    "revision": "e7c398ff0b2d375437da",
+    "url": "/portfolio/static/js/runtime-main.eeda1979.js"
+  },
+  {
+    "revision": "a8817aef7334c7d1516e526bb8be159d",
+    "url": "/portfolio/static/media/1.a8817aef.png"
+  },
+  {
+    "revision": "b4292cb6be85529cd9db305f1bae5f26",
+    "url": "/portfolio/static/media/2.b4292cb6.png"
+  },
+  {
+    "revision": "24884fb57e2703f034f569696317c177",
+    "url": "/portfolio/static/media/3.24884fb5.png"
+  },
+  {
+    "revision": "cb16091c6c4dfa1e66fc291b3d9ae82b",
+    "url": "/portfolio/static/media/4.cb16091c.png"
+  },
+  {
+    "revision": "50909547060b19350f3f386129e74ddd",
+    "url": "/portfolio/static/media/5.50909547.png"
+  },
+  {
+    "revision": "efd831dcf203eb6cde7ccfcf86b06074",
+    "url": "/portfolio/static/media/6.efd831dc.png"
+  },
+  {
+    "revision": "67ce868a6d46cd468370c04dad3d93fa",
+    "url": "/portfolio/static/media/Pixel_Times.67ce868a.ttf"
+  },
+  {
+    "revision": "f061c5e077210e94f0ede8f8891b994a",
+    "url": "/portfolio/static/media/devil.f061c5e0.png"
+  },
+  {
+    "revision": "4a40da0f36ba717a4bb4f9655ebcfd09",
+    "url": "/portfolio/static/media/est.4a40da0f.png"
+  },
+  {
+    "revision": "a301e16e82f6c072ac15ab55625bcdf3",
+    "url": "/portfolio/static/media/ghost.a301e16e.png"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/portfolio/static/media/telegram.a888fec0.svg"
+  }
+]);
